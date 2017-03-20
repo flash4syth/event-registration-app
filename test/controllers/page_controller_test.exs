73e6cfec09d3f68fdba2c6b8accd2adc47717dbb
Jon Forsyth <jon4syth@gmail.com>
2017-03-20 14:58:36 -0600

    Install hound; Pass ctrl,end2end tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,10 @@
 defmodule SR.PageControllerTest do
   use SR.ConnCase
 
+  @moduletag :controller_test
+
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Stake Retreat"
   end
 end
