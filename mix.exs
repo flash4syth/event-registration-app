f23f30a504c20bda5b183ca433f274f059ffa996
Jon Forsyth <jon4syth@gmail.com>
2017-03-28 17:41:14 -0600

    Update phoenix and phoenix_ecto pkgs

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule SR.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
+      {:phoenix, "~> 1.2.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
