0198c9b7f4c02d674fe51116bbb3033d4021012a
Jon Forsyth <jon4syth@gmail.com>
2017-05-06 21:31:37 -0600

    Update Meal View

--- web/elm/Views/MealView.elm
+++ web/elm/Views/MealView.elm
@@ -14,35 +14,35 @@ view model =
     section [ class "row" ]
         (addLogo
             :: [ header []
-                    [ h1 [ style Styles.centerText ] [ text "Meal Schedule" ] ]
-               , table
-                    [ class "table table-bordered table-striped" ]
-                    [ thead []
-                        [ tr []
-                            [ th [] [ text "Date" ]
-                            , th [] [ text "Location" ]
-                            , th [] [ text "Description" ]
-                            ]
-                        ]
-                    , tbody []
-                        (makeRows model)
+                    [ h1 [ style Styles.centerText ] [ text "Meals" ] ]
+               , dl [ style Styles.mediumText ]
+                    -- Dinner
+                    [ dt [] [ text "Dinner - Fresh Mex Salads and Burritos" ]
+                    , dd [] [ text "Sweet Pork and Lime Chicken" ]
+                    , dd [] [ text "Fiesta Rice and Black Beans" ]
+                    , dd [] [ text "Flour Tortillas" ]
+                    , dd [] [ text "Shredded Romaine, Sour Cream and Pico d' gallo" ]
+                    , dd [] [ text "Cheddar Jack Cheese, Cilantro Ranch and Tortilla Chips" ]
+                    , dd [] [ text "Chocolate Caramel Cake" ]
+                    , dd [] [ text "Lemon Cream Cake" ]
+                      -- Breakfast
+                    , dt [] [ text "Breakfast" ]
+                    , dd [] [ text "French Toast Bake with Buttermilk Syrup" ]
+                    , dd [] [ text "Savory Sausage Egg Bake" ]
+                    , dd [] [ text "Ketchup and Hot Sauce" ]
+                    , dd [] [ text "Fresh Fruit Platter" ]
+                    , dd [] [ text "Milk" ]
+                    , dd [] [ text "Orange Juice" ]
+                      -- Lunch
+                    , dt [] [ text "BBQ Lunch" ]
+                    , dd [] [ text "Pulled Pork and BBQ Beef" ]
+                    , dd [] [ text "Dutch Oven Potatoes" ]
+                    , dd [] [ text "Spinach Salad" ]
+                    , dd [] [ text "White Dinner Rolls and Butter" ]
+                    , dd [] [ text "Snickerdoodles" ]
+                    , dd [] [ text "Chocolate Chip Cookies" ]
+                    , dd [] [ text "Lemonade" ]
                     ]
-                 --  , (if model.userType == AdminUser then
-                 --       if model.editModeActive then
-                 --           button
-                 --               [ class "btn btn-info btn-lg"
-                 --               , onClick UpdateFood
-                 --               ]
-                 --               [ text "Save" ]
-                 --       else
-                 --           button
-                 --               [ class "btn btn-warning btn-lg"
-                 --               , onClick ToggleEditMode
-                 --               ]
-                 --               [ text "Edit Meals" ]
-                 --     else
-                 --       p [] []
-                 --    )
                ]
         )
 
